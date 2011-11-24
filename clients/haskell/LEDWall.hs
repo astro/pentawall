4505db788b2ff0185e05f541b9a1a5affad8d7ac
Astro <astro@spaceboyz.net>
2011-11-24 02:04:59 +0100

    LEDWall: Monoid Color, RGB clamping

--- clients/haskell/LEDWall.hs
+++ clients/haskell/LEDWall.hs
@@ -3,11 +3,18 @@ module LEDWall where
 import Text.Printf
 import Control.Monad.State.Lazy
 import Data.Word (Word8)
+import Data.Monoid
 
 type Word = Word8
 
 data Color = RGB Word Word Word
            | RGBDouble Double Double Double
+           deriving (Show, Eq)
+             
+instance Monoid Color where
+  mempty = RGB 0 0 0
+  mappend (RGB r g b) (RGB r' g' b') = RGB (r + r') (g + g') (b + b')
+             
 type Position = (Word, Word)
 
 data Instruction = Blank Color
@@ -29,7 +36,8 @@ command2 (x, y) (RGB r g b) =
 command3 colors = "03" ++ concatMap colorToHex colors ++ "\r\n"
 
 colorToHex :: Color -> String
-colorToHex (RGB r g b) = printf "%02x%02x%02x" r g b
+colorToHex (RGB r g b) = printf "%02x%02x%02x" (clamp r) (clamp g) (clamp b)
+    where clamp = min 255 . max 0
 colorToHex (RGBDouble r g b) = colorToHex $
                                RGB (c r) (c g) (c b)
     where c :: Double -> Word
